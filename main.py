c135312d1c27f5b367a4fd911645dd899ab96dac
chaya-nager <chaya4169225@gmail.com>
2025-02-25 19:23:53 +0200

    change

--- main.py
+++ main.py
@@ -1,25 +1,82 @@
-# 3.
-from circle import Circle
-from square import Square
+# 1
+def func1(num1, num2,num3):
+    if num1 > num2:
+        return num1+num3
+    return num2
 
 
-def equals_perimeters(shapes):
-    scope = shapes[0].calc_perimeter()
-    for i in range(1, len(shapes)):
-        if shapes[i].calc_perimeter() != scope:
-            return False
-    return True
+print(func1(10, 20))
 
 
-# 4.
-def is_shape_exist(shapes, shape):
-    if shape in shapes:
-        return True
-    return False
+# 2
+def func2(num1, num2, num3):
+    x = func1(num1, num2)
+    if x > num3:
+        return x
+    else:
+        return num3
 
 
-# 5.
-def multi_shapes(shapes):
-    return [shape+ shape for shape in shapes]
+print(func2(2, 5, 1))
 
 
+# 3
+def func3(num):
+    newnum = num % 10 * 100 + num // 10 % 10 * 10 + num // 100
+    return abs(newnum - num)
+
+
+def abs(n):
+    if n < 0:
+        return n * -1
+    else:
+        return n
+
+
+print(func3(123))
+
+
+# 4
+def func4(num):
+    x = num // 10 % 10 * 10 + num % 10
+    if x < 50:
+        return num - x
+    return num + 100 - x
+
+
+print(func4(453))
+
+
+# 5
+def days_in_month(month):
+    if month == 2:
+        return 28
+    if month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    else:
+        return 31
+
+
+print(days_in_month(5))
+
+
+# 6
+def tomorrow(date):
+    days = days_in_month(date // 100 % 10)
+    print(date // 10000)
+    if date // 10000 == days:
+        return 1 * 10000 + (date // 100 % 10 + 1) * 100 + date // 10 % 10 * 10 + date % 10
+    return date + 10000
+
+
+print(tomorrow(280224))
+
+#7
+def func7(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return func7(n - 1) + func7(n - 2)
+
+print(func7(9))
